9a11b6767adc9c9704794ca675172ccc5d159e08
ChiobiJason <chiobichisom@gmail.com>
2022-07-09 08:54:21 +0000

    Finished website for now

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+//Initialize AOS
+AOS.init();
+
+
+//Upcoming Event Timer
+const countdown = () => {
+    const countDate = new Date('July 10, 2022 08:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    //Time calculation
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day)/ hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
+
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
