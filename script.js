1cec09d4dc741fdd7a1a530d4aac426cafc57af6
ChiobiJason <chiobichisom@gmail.com>
2022-12-29 14:36:36 -0700

    Fixed Date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ AOS.init();
 
 //Upcoming Event Timer
 const countdown = () => {
-  const countDate = new Date("December 25, 2022 08:00:00").getTime();
+  const countDate = new Date("January 1, 2023 08:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
