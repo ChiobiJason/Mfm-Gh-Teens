d83949f48230ae39676597da5f5eee87ed962b70
ChiobiJason <chiobichisom@gmail.com>
2023-01-27 09:45:52 -0700

    Changed Date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ AOS.init();
 
 //Upcoming Event Timer
 const countdown = () => {
-  const countDate = new Date("January 1, 2023 08:00:00").getTime();
+  const countDate = new Date("January 29, 2023 08:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
